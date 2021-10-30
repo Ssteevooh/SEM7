146753ae67796ff14ac1e0bf5d6ccd510889524d
JarkkoJake <71126476+JarkkoJake@users.noreply.github.com>
2021-10-30 23:35:23 +0300

    SEM7-83 dropdown item requesting
    Added endpoints to request dropdown items. currently reposing with just a test message

--- backend/controllers/browseController.js
+++ backend/controllers/browseController.js
@@ -1,3 +1,4 @@
+// -----Auction browsing --------
 
 // send all information about the auction for a detailed view
 
@@ -14,5 +15,37 @@ exports.browseListView = (req, res) => {
 // send information for tile view about the auctions (thumbnail image, starting and selling prices, year, company)
 
 exports.browseTileView = (req, res) => {
-    res.send("page " + req.query.page + " of auctions ordered by " + req.query.orderby);
+    res.send("page " + (req.query.page || 1) + " of auctions ordered by " + (req.query.orderby || "date"));
+};
+
+// -------- Dropdown items --------
+
+// get all sellers for dropdown menu
+
+exports.getSellers = (req, res) => {
+    res.send("all sellers");
+};
+
+// get all countries for dropdown menu
+
+exports.getCountries = (req, res) => {
+    res.send("all countries");
+};
+
+// get all categories under a country specified on the query
+
+exports.getCategories1 = (req, res) => {
+    res.send("all categories under " + req.params.country);
+};
+
+// get all category 2's under a category1 specified on the query
+
+exports.getCategories2 = (req, res) => {
+    res.send("all category 2's under " + req.params.category1);
+};
+
+// get all category 3's under a category2 specified on the query
+
+exports.getCategories3 = (req, res) => {
+    res.send("all category 3's under " + req.params.category2);
 };
--- backend/routes/browseRoutes.js
+++ backend/routes/browseRoutes.js
@@ -2,6 +2,8 @@ const express = require('express'),
     router = express.Router(),
     browseController = require('../controllers/browseController');
 
+// -----Auction browsing-------
+
 // used to get detailed information from one auction using its id
 router.get('/detailed/:id', browseController.browseWithId);
 
@@ -10,5 +12,22 @@ router.get('/listView', browseController.browseListView);
 
 // used to get information for tile based view, search options as query
 router.get('/tileView', browseController.browseTileView);
+
+// -------Dropdown items -----------
+
+// get sellers
+router.get('/sellers', browseController.getSellers);
+
+// get countries
+router.get('/countries', browseController.getCountries);
+
+// get cat1's under a specific country
+router.get('/category1/:country', browseController.getCategories1);
+
+// get cat2's under a specific cat1
+router.get('/category2/:category1', browseController.getCategories2);
+
+// get cat3's under a specific cat2
+router.get('/category3/:category2', browseController.getCategories3);
 
 module.exports.router = router;
