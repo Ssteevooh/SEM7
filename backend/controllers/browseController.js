d57c7fbbffb313e061398d7d305bf727e6a96dc3
JarkkoJake <71126476+JarkkoJake@users.noreply.github.com>
2021-10-30 23:45:06 +0300

    SEM7-83 dropdowns moved
    Moved dropdown related code to their own router and controller to avoid modules growing too much in the future

--- backend/controllers/browseController.js
+++ backend/controllers/browseController.js
@@ -1,5 +1,3 @@
-// -----Auction browsing --------
-
 // send all information about the auction for a detailed view
 
 exports.browseWithId = (req, res) => {
@@ -18,34 +16,3 @@ exports.browseTileView = (req, res) => {
     res.send("page " + (req.query.page || 1) + " of auctions ordered by " + (req.query.orderby || "date"));
 };
 
-// -------- Dropdown items --------
-
-// get all sellers for dropdown menu
-
-exports.getSellers = (req, res) => {
-    res.send("all sellers");
-};
-
-// get all countries for dropdown menu
-
-exports.getCountries = (req, res) => {
-    res.send("all countries");
-};
-
-// get all categories under a country specified on the query
-
-exports.getCategories1 = (req, res) => {
-    res.send("all categories under " + req.params.country);
-};
-
-// get all category 2's under a category1 specified on the query
-
-exports.getCategories2 = (req, res) => {
-    res.send("all category 2's under " + req.params.category1);
-};
-
-// get all category 3's under a category2 specified on the query
-
-exports.getCategories3 = (req, res) => {
-    res.send("all category 3's under " + req.params.category2);
-};
--- backend/main.js
+++ backend/main.js
@@ -1,10 +1,14 @@
+//const { request } = require('http');
+
 const express = require('express'), app = express(), port = 5000,
     browseRouter = require('./routes/browseRoutes'),
     postRouter = require('./routes/postRoutes'),
+    dropdownRouter = require('./routes/dropdownRoutes'),
     loginRouter = require('./routes/loginRoutes');
 
 app.use('/browse', browseRouter.router);
 app.use('/post', postRouter.router);
+app.use('/dropdown', dropdownRouter.router);
 
 app.listen(port, () => {
     console.log('Server running on port ' + port);
