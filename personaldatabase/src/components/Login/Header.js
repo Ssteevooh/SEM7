1d657afe9abc3d353f643ca327eb6a7508ae8f34
Pekka Lehtola <pekka.lehtola98@gmail.com>
2021-10-30 22:46:33 +0300

    Initial logic for loginpage

--- personaldatabase/src/components/Login/Header.js
+++ personaldatabase/src/components/Login/Header.js
@@ -1,21 +1,49 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from "react";
+import { Input } from "antd";
+import PropTypes from "prop-types";
+import "./Login.css";
 
 const Header = ({ title }) => {
-    return (
-        <header className='header'>
-            <h1 id="rcorners1">{title}</h1>
-            <button className='loginButton'>LOGIN</button>
-        </header>
-    )
-}
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const submit = (err) => {
+    if (username && password) {
+      console.log(username.target.value);
+      console.log(password.target.value);
+    } else {
+      console.log("Error");
+    }
+  };
+
+  return (
+    <body className="header">
+      <h1 id="rcorners1">{title}</h1>
+
+      <Input
+        id="username"
+        placeholder={"Username..."}
+        onChange={(value) => setUsername(value)}
+      />
+      <Input
+        id="password"
+        placeholder={"Password..."}
+        onChange={(value) => setPassword(value)}
+      />
+
+      <button className="loginButton" onClick={() => submit()}>
+        LOGIN
+      </button>
+    </body>
+  );
+};
 
 Header.defaultProps = {
-    title: "Stamp auction database",
-}
+  title: "Stamp auction database",
+};
 
 Header.protoTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
-export default Header
+export default Header;
