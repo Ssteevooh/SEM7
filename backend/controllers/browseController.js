1f607679abffb0a6c9f243fbfef21872d180d6f9
JarkkoJake <71126476+JarkkoJake@users.noreply.github.com>
2021-11-04 10:37:55 +0200

    changed queries to bodies in browse controller

--- backend/controllers/browseController.js
+++ backend/controllers/browseController.js
@@ -7,12 +7,12 @@ exports.browseWithId = (req, res) => {
 // send information for list view about the auctions (thumbnail image, catalog number, description, starting and selling prices, year, company)
 
 exports.browseListView = (req, res) => {
-    res.send("page " + (req.query.page || 1) + " of auctions ordered by " + (req.query.orderby || "date"));
+    res.send("page " + (req.body.page || 1) + " of auctions ordered by " + (req.body.orderby || "date"));
 };
 
 // send information for tile view about the auctions (thumbnail image, starting and selling prices, year, company)
 
 exports.browseTileView = (req, res) => {
-    res.send("page " + (req.query.page || 1) + " of auctions ordered by " + (req.query.orderby || "date"));
+    res.send("page " + (req.body.page || 1) + " of auctions ordered by " + (req.body.orderby || "date"));
 };
 
