6a66a17e131555baa8a15f49b00adbfa7de535ea
Pekka Lehtola <pekka.lehtola98@gmail.com>
2021-11-04 13:53:29 +0200

    Trying to make merge error

--- personaldatabase/src/components/Login/Header.js
+++ personaldatabase/src/components/Login/Header.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
 const Header = ({ title }) => {
-    return (
-        <header className='header'>
-            <h1 id="rcorners1">{title}</h1>
-            <button className='loginButton'>LOGIN</button>
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <h1 id="rcorners1">{title}</h1>
+      <div className="loginButton">LOGIN</div>
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: "Stamp auction database",
-}
+  title: "Stamp auction database",
+};
 
 Header.protoTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
-export default Header
+export default Header;
