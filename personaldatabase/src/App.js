7d3f9c8bd51d2a560a32c4001ddc700c5777d57e
Pekka Lehtola <pekka.lehtola98@gmail.com>
2021-11-08 20:46:30 +0200

    SEM7-84: Implemented Eslint

--- personaldatabase/src/App.js
+++ personaldatabase/src/App.js
@@ -1,10 +1,11 @@
 import { LoginProvider } from "./Contexts/LoginContext";
 import { RouteProvider } from "./Contexts/RouterContext";
+import React from "react";
 
 const App = () => {
   return (
     <LoginProvider>
-      <div className="container">
+      <div className='container'>
         <RouteProvider />
       </div>
     </LoginProvider>
--- personaldatabase/src/contexts/LoginContext.js
+++ personaldatabase/src/contexts/LoginContext.js
@@ -12,13 +12,13 @@ export const LoginProvider = ({ children }) => {
   const loginCall = async (user, pass) => {
     // TODO:  better logic
     //        Change adress to backend
-    console.log("test");
     const response = await axios
       .get("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
         const person = res.data;
         console.log(person);
       })
+
       .then(() => {
         return dummyUser.username === user || user === "."
           ? dummyUser.password === pass || pass === "."
