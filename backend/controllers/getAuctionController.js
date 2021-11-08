a50c388471112f006af532ee56f1634950440eba
JarkkoJake <71126476+JarkkoJake@users.noreply.github.com>
2021-11-08 18:03:10 +0200

    endpoints fixed
    Fixed endpoints to match the communication documentation

--- backend/controllers/getAuctionController.js
+++ backend/controllers/getAuctionController.js
@@ -1,16 +1,16 @@
 // send all information about the auction for a detailed view
 
 exports.browseWithId = (req, res) => {
-    res.send("All information about the auction " + req.params.id + " in detailed view");
+    res.send("All information about the auction " + req.body.id + " in detailed view");
 };
 
-// send information for list view about the auctions (thumbnail image, catalog number, description, starting and selling prices, year, company)
+// send information for list view about the auctions
 
 exports.browseListView = (req, res) => {
     res.send("Page " + (req.body.page || 1) + " of auctions ordered by " + (req.body.orderby || "date"));
 };
 
-// send information for tile view about the auctions (thumbnail image, starting and selling prices, year, company)
+// send information for tile view about the auctions
 
 exports.browseTileView = (req, res) => {
     res.send("Page " + (req.body.page || 1) + " of auctions ordered by " + (req.body.orderby || "date"));
