e356e9e7842d5c5b50a381ba140ca3193752e94b
Jarkko Heinonen <71126476+JarkkoJake@users.noreply.github.com>
2021-11-21 19:49:06 +0200

    setting up auction routes
    +1

--- backend/main.js
+++ backend/main.js
@@ -1,13 +1,13 @@
 //const { request } = require('http');
 
 const express = require('express'), app = express(), port = 5000,
-    browseRouter = require('./routes/auctionRoutes'),
+    auctionRouter = require('./routes/auctionRoutes'),
     dropdownRouter = require('./routes/dropdownRoutes'),
     loginRouter = require('./routes/loginRoutes');
 
 app.use(express.json());
 
-app.use('/auctions', browseRouter.router);
+app.use('/auctions', auctionRouter.router);
 app.use('/dropdown', dropdownRouter.router);
 app.use("/login", loginRouter.Router);
 
